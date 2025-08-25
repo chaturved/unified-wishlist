b8a449c1619309a19313f202f0800ae1712cf363
Chaturved Sumanth Lakkaraju <chaturvedsumanthlakkaraju@gmail.com>
2025-08-25 15:38:04 -0500

    init

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
